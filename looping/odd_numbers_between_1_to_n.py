a7875907ce609c9f3a2e85f2592400c2ccd1c4b3
padma67 <60962682+padma67@users.noreply.github.com>
2020-02-13 07:28:19 +0530

    Create odd_numbers_between_1_to_n.py

---
+++ looping/odd_numbers_between_1_to_n.py
@@ -0,0 +1,11 @@
+#To print all odd numbers between 1 to n
+
+#get the n value from user
+n=int(input("Enter the n value"))
+#declare the list for collect odd numbers
+odd=[]
+for i in range(1,n):
+    if(i%2!=0):
+        odd.append(i)
+print("odd numbers between 1 to n",odd)
+      
