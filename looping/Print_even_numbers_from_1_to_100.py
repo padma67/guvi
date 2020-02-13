7b7c6440c8c9d8b8ceef1f246929d79a4c06a66e
padma67 <60962682+padma67@users.noreply.github.com>
2020-02-13 20:34:16 +0530

    Create Print_even_numbers_from_1_to_100.py

---
+++ looping/Print_even_numbers_from_1_to_100.py
@@ -0,0 +1,13 @@
+#To print all odd numbers between 1 to 100
+
+def Even():
+    #declare the list for collect even numbers
+    even=[]
+    i=1
+    while(i<=100):
+        if(i%2==0):
+            even.append(i)
+        i=i+1
+    print("even numbers between 1 to n",even)
+Even()
+      
