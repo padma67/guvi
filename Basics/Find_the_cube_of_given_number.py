55dd25455837a4642fff94114bad6549f26bafbb
padma67 <60962682+padma67@users.noreply.github.com>
2020-02-16 10:26:24 +0530

    Create Find_the_cube_of_given_number.py

---
+++ Basics/Find_the_cube_of_given_number.py
@@ -0,0 +1,9 @@
+#Find the cube of given number
+def cube():
+    #get the number from user
+    A = int(input())
+    # calculate the cube of a numebr
+    Cube=A**3
+    return Cube
+res = cube()
+print(res)
