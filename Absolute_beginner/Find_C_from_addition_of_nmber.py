9170bb76ccda4b192a65b81357c92c2d51596634
padma67 <60962682+padma67@users.noreply.github.com>
2020-02-16 14:06:07 +0530

    Create Find_C_from_addition_of_nmber.py

---
+++ Absolute_beginner/Find_C_from_addition_of_nmber.py
@@ -0,0 +1,9 @@
+# You are given Two Numbers, A and B. If C = A + B. Find C.
+def Add():
+    #get A and B value from user
+    A=int(input())
+    B=int(input())
+    C=A+B
+    return C
+res=Add()
+print(res)
