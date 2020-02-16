28831a06ab8cdf0fe947d7e067dd40f0dc1676a5
padma67 <60962682+padma67@users.noreply.github.com>
2020-02-16 13:26:54 +0530

    Create Find_GCD_of_given_nmber.py

---
+++ Basics/Find_GCD_of_given_nmber.py
@@ -0,0 +1,13 @@
+#Given 2 numbers N,M find the GCD of N and M.If it cannot be found for given number(s) then print -1
+def GCD():
+    gcd=0
+    g=-1
+    N, M = map(int, input().split())
+    i = 1
+    while(i <= N and i <= M):
+        if(N % i == 0 and M % i == 0):
+            gcd = i
+        i = i + 1
+    
+res=GCD()
+print(res)
