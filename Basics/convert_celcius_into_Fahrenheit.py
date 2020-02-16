5ba589871c156abda11248771d927991a5c58d43
padma67 <60962682+padma67@users.noreply.github.com>
2020-02-16 13:17:03 +0530

    Delete convert_celcius_into_Fahrenheit.py

--- Basics/convert_celcius_into_Fahrenheit.py
+++
@@ -1,8 +0,0 @@
-#Write a program to convert celcius into Fahrenheit.
-def celcius():
-    A=float(input())
-    faranheat= float((A*9/5)+32)
-    fh=round(faranheat,2)
-    return fh
-res=celcius()
-print(res)
