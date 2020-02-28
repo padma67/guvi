777273da649d28ae2d89e12a4d0325c78e425f87
padma67 <60962682+padma67@users.noreply.github.com>
2020-02-28 12:51:35 +0530

    Create raed_string_right_to_left_and_left_to_right_check_palindrome.py

---
+++ Strings/raed_string_right_to_left_and_left_to_right_check_palindrome.py
@@ -0,0 +1,10 @@
+my_str=input()
+# make it suitable for caseless comparison
+my_str = my_str.casefold()
+# reverse the string
+rev_str = reversed(my_str)
+# check if the string is equal to its reverse
+if list(my_str) == list(rev_str):
+   print("1")
+else:
+   print("0")
