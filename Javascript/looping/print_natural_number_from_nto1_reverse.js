8f69297a57f52a19473e9481ab298fe18f7f0b3b
padma67 <60962682+padma67@users.noreply.github.com>
2020-02-28 20:07:59 +0530

    Create print_natural_number_from_nto1_reverse.js

---
+++ Javascript/looping/print_natural_number_from_nto1_reverse.js
@@ -0,0 +1,9 @@
+$print natural numbers from n to 1 in reverse order
+$get n value from user
+var n=prompt();
+$check the condition print natural numbers from n to 1 in reverse order
+while(n>0)
+{
+    console.log(n);
+    n=n-1;
+}
