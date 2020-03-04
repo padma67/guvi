c80bfb44d9f2e0a8e2a565e4d8ed168c6e369b2a
padma67 <60962682+padma67@users.noreply.github.com>
2020-03-04 16:33:18 +0530

    Update Find_3_multiple_of_given_number.py

--- Absolute_beginner/Find_3_multiple_of_given_number.py
+++ Absolute_beginner/Find_3_multiple_of_given_number.py
@@ -1,6 +1,9 @@
 #Print the First 3 multiples of the given number "N".
 #get A  value from user
-A=int(input())
-B=A+A
-C=A+A+A
-print(A,B,C)
+def multiple():
+  A=int(input())
+  B=A+A
+  C=A+A+A
+  return A,B,C
+res=multiple();
+print(res)
