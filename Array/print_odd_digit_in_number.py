e54a2df3a076f6609d46e4f04ee6a7a0a302b784
padma67 <60962682+padma67@users.noreply.github.com>
2020-03-04 17:11:52 +0530

    Create print_odd_digit_in_number.py

---
+++ Array/print_odd_digit_in_number.py
@@ -0,0 +1,11 @@
+def sumdig():
+    n=int(input())
+    sum=0
+    while(n>0):
+        dig=n%10
+        if(dig%2==0):
+          print(deg)
+        n=n//10
+    else:
+      print("-1")
+sumdig()
