43656a2e4938e64cd19328bd5d54cabea122ba21
padma67 <60962682+padma67@users.noreply.github.com>
2020-03-04 16:39:16 +0530

    Update Find_if_3_sides_form_triangle.py

--- Absolute_beginner/Find_if_3_sides_form_triangle.py
+++ Absolute_beginner/Find_if_3_sides_form_triangle.py
@@ -1,10 +1,13 @@
-# function to check if three sides form a triangle or not  
-A, B,C = map(int, input().split())
-y="yes"
-n="no"
-# check condition  
-if (A + B <= C) or (A + C <= B) or (B + C <= A):
+# function to check if three sides form a triangle or not 
+def triangle():
+	A, B,C = map(int, input().split())
+	y="yes"
+	n="no"
+	# check condition  
+	if (A + B <= C) or (A + C <= B) or (B + C <= A):
 	print(y)
-else: 
-    print(n)
+	else: 
+    	print(n)
+res=triangle()
+print(res)
     
