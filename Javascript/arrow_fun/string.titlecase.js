b459ff458d8bc8cd204cc5b155cb1ad3ad1e29bf
padma67 <60962682+padma67@users.noreply.github.com>
2020-03-06 16:08:01 +0530

    Update string.titlecase.js

--- Javascript/arrow_fun/string.titlecase.js
+++ Javascript/arrow_fun/string.titlecase.js
@@ -1,3 +1,3 @@
 var a=['harish','sam','jack','amir'];
-var tit=a.map(item)=>{return item[0].toUpperCase()+item.substr(1).toLowerCase()};
+var tit=a.map(item)=>( item[0].toUpperCase()+item.substr(1).toLowerCase());
 console.log(tit);
