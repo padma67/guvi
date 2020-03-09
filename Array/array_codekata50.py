187cd713ab386dd575f340873df1209553b35705
padma67 <60962682+padma67@users.noreply.github.com>
2020-03-09 15:12:51 +0530

    Update array_codekata50.py

--- Array/array_codekata50.py
+++ Array/array_codekata50.py
@@ -1,4 +1,8 @@
-n=int(input())
-a=list(map(int,input().split()))
-b=sorted(a)
-print(b[len(b)-2])
+def max():
+  n=int(input())
+  a=list(map(int,input().split()))
+  b=sorted(a)
+  c=(b[len(b)-2])
+  return c
+res=max()
+console.log(res)
