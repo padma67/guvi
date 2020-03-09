7d95d4fd822c3b4c43246751c8f9ee76a1b4d565
padma67 <60962682+padma67@users.noreply.github.com>
2020-03-09 17:24:36 +0530

    Update find_sum_of_first_digit_last_digit.py

--- Array/find_sum_of_first_digit_last_digit.py
+++ Array/find_sum_of_first_digit_last_digit.py
@@ -3,8 +3,12 @@
 def fldigit():
     number = int(input())
     firstdigit = number
+    #find the firstdigit of number
     while (firstdigit >= 10):
       firstdigit = firstdigit // 10
+    #find the lastdigit of number
     lastdigit = number % 10
-    print(firstdigit+lastdigit)
-fldigit()
+    sum=(firstdigit+lastdigit)
+    return sum
+res=fldigit()
+print(res)
