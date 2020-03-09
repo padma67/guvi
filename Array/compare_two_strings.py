391fb71453bdb4d683b79b91a88e99927b68515e
padma67 <60962682+padma67@users.noreply.github.com>
2020-03-09 17:06:25 +0530

    Update compare_two_strings.py

--- Array/compare_two_strings.py
+++ Array/compare_two_strings.py
@@ -1,12 +1,18 @@
 #Given 2 strings S1 and s2, check whether they are case senitively equal without using any predefined function(case sensitive).
 #If they are not same print 'no'
-s1,s2=map(str,input().split())
-count=0
-if(len(s1)==len(s2)):
-	for i in range(len(s1)):
-		if(s1[i]==s2[i]):
-			count+=1
-if(count==len(s2)):
-	print("yes")
-else:
-	print("no")
+def str():
+	s1,s2=map(str,input().split())
+	count=0
+	y="yes"
+	n="no"
+	#check two strings are case senitively equal
+	if(len(s1)==len(s2)):
+		for i in range(len(s1)):
+			if(s1[i]==s2[i]):
+				count+=1
+	if(count==len(s2)):
+		return y
+	else:
+		return n
+res=str()
+print(res)
