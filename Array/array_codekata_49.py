dcb388b773fc4f9b35b8de59dbf5837dade003ba
padma67 <60962682+padma67@users.noreply.github.com>
2020-03-09 16:39:14 +0530

    Update array_codekata_49.py

--- Array/array_codekata_49.py
+++ Array/array_codekata_49.py
@@ -1,9 +1,16 @@
-N=int(input())
-num1=list(map(int,input().split()))
-m=sorted(num1)
-a=m[::-1]
-s=[]
-for i in range(N):
-	s.append(a[i])
-	s.append(m[i])
-print(*s[:N])
+#Alternate sorting:Given a number N followed by array of N elements,
+#sort the array in such a way that the first number is the first maximum and second number is the 1st minimum 3rd number isthe 2nd maximum and so on.
+def max():
+	N=int(input())
+	num1=list(map(int,input().split()))
+	m=sorted(num1)
+	a=m[::-1]
+	s=[]
+	v=[]
+	for i in range(N):
+		s.append(a[i])
+		s.append(m[i])
+	v=(*s[:N])
+	return v
+res=max()
+print(res)
