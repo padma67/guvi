c2e76e248fb82a8a148d96559f7f638450a106ad
padma67 <60962682+padma67@users.noreply.github.com>
2020-03-09 15:19:49 +0530

    Update array_codekata84.py

--- Array/array_codekata84.py
+++ Array/array_codekata84.py
@@ -1,12 +1,17 @@
-S=input()
-l=len(S)
-d=float(l/2)
-D=round(d)
-if(l%2==0):
-  S.replace(d,"*")
-  S.replace(d+1,"*")
-  print(S)
-else:
-  S.replace(d,"*")
-  print(S)
+#print two star between string,if its length odd,else print one star between string
+#get the input from user
+def replace():
+  S=input()
+  l=len(S)
+  d=float(l/2)
+  D=round(d)
+  if(l%2==0):
+    S.replace(d,"*")
+    S.replace(d+1,"*")
+    return S
+  else:
+    S.replace(d,"*")
+    return S
+  res=replace()
+  print(res)
   
