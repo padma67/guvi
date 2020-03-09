b597f178fbcd548a1209c8d6b4b4dd67c94b723e
padma67 <60962682+padma67@users.noreply.github.com>
2020-03-09 17:31:31 +0530

    Update print_odd_digit_in_number.py

--- Array/print_odd_digit_in_number.py
+++ Array/print_odd_digit_in_number.py
@@ -1,11 +1,15 @@
+#print odd digit in a number
 def sumdig():
     n=int(input())
     sum=0
     while(n>0):
+        #find each digit of number
         dig=n%10
-        if(dig%2==0):
-          print(deg)
+        #check if the digit odd or even
+        if(dig%2!=0):
+          return dig
         n=n//10
     else:
-      print("-1")
-sumdig()
+     return -1
+res=sumdig()
+print(res)
