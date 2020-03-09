adb78e0659087a295c7cd72ffeace1e818595c3e
padma67 <60962682+padma67@users.noreply.github.com>
2020-03-09 16:54:51 +0530

    Update array_is_majestic_or_not.py

--- Array/array_is_majestic_or_not.py
+++ Array/array_is_majestic_or_not.py
@@ -1,12 +1,19 @@
-N=int(input())
-arr = list(map(int,input().split()))
-fsum=0
-lsum=0
-for i in range(1,3):
-  fsum=fsum+arr[i]
-for i in range(N-4,N+1):
-  lsum=fsum+arr[i]
-if(fsum==lsum):
-  print("1")
-else:
-  print("0")
+#check given array is majestic or not.A ‘majsetic’ array is an array whose sum of first three number is equal to last three number.
+def arr():
+  N=int(input())
+  arr = list(map(int,input().split()))
+  fsum=0
+  lsum=0
+  #using for loop to find sum of first three numbers in array
+  for i in range(1,3):
+    fsum=fsum+arr[i]
+  #using for loop to find sum of first three numbers in array
+  for i in range(N-4,N+1):
+    lsum=fsum+arr[i]
+  #compare the sum value
+  if(fsum==lsum):
+    return 1
+  else:
+    return 0
+res=arr()
+print(res)
