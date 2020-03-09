9c92be806364245e07e66eddc04305097a523fe7
padma67 <60962682+padma67@users.noreply.github.com>
2020-03-09 17:37:52 +0530

    Update sum_of_odd_numbers_between_L_R.py

--- Array/sum_of_odd_numbers_between_L_R.py
+++ Array/sum_of_odd_numbers_between_L_R.py
@@ -4,8 +4,10 @@ def sumodd():
     #get the n value from user
     L,R= map(int, input().split())
     sum=0
+    #find odd numbers between L and R by using for loop
     for i in range(L,R+1):
       if(i%2!=0):
         sum=sum+i
-    print(sum)
-sumodd()
+    return sum
+res=sumodd()
+print(res)
