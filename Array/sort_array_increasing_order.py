3e96154f4ff1d287757475dd9bd108691c7b2113
padma67 <60962682+padma67@users.noreply.github.com>
2020-03-09 17:35:33 +0530

    Update sort_array_increasing_order.py

--- Array/sort_array_increasing_order.py
+++ Array/sort_array_increasing_order.py
@@ -1,9 +1,15 @@
-N=int(input()) 
-n1=list(map(int,input().split())) 
-n2=sorted(n1) 
-a=[]
-for i in range(N):
-    for j in range(N): 
-        if(n2[i]==n1[j]):
-             a.append(j+1) 
-print(*a) 
+#sort the array in increasing order
+def arr():
+    N=int(input()) 
+    n1=list(map(int,input().split())) 
+    #sort the array by inbuild function
+    n2=sorted(n1) 
+    a=[]
+    for i in range(N):
+        for j in range(N): 
+            if(n2[i]==n1[j]):
+                a.append(j+1) 
+    c=*a
+    return c
+res=arr()
+print(res)
