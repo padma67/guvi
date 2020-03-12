bceb97ec8bda574c148c56920b4ca6c4e63fc16b
padma67 <60962682+padma67@users.noreply.github.com>
2020-03-12 20:51:18 +0530

    Create print_the_distinct_pairs_formed_by_multiplying_two_prime_numbers.js

---
+++ Javascript/codekata/mathematics/print_the_distinct_pairs_formed_by_multiplying_two_prime_numbers.js
@@ -0,0 +1,48 @@
+//Given a number N, print the distinct pairs formed by multiplying two prime numbers (i.e)prime x prime should yield the N.
+//Also print the numbers in descending order.If no such pairs can be formed print '-1'.
+const readline = require ('readline');
+const inp=readline.createInterface({
+  input:process.stdin
+});
+inp.on("line",(data)=>{
+  var b=[];
+  var c=0;
+  var d=0;
+  var e=0;
+  for(i=2;i<=data;i++)
+  { 
+    var count=0;
+    for(j=1;j<=i;j++)
+    {
+      if(i%j==0)
+      {
+        count=count+1;
+      }
+    }
+    if(count==2)
+    {
+      b.push(i);
+    }
+  }
+  for(i=0;i<b.length;i++)
+  {
+    for(j=i+1;j<b.length;j++)
+    {
+      if(b[i]*b[j]==data)
+      {
+        d=b[i];
+        e=b[j];
+        c=c+1;
+        break;
+      }
+    }
+  }
+
+  if(c==1){
+  console.log(e,d);
+    }
+  else
+  {
+    console.log("-1");
+  }
+});
