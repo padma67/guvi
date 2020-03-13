82c396646270a474761fed10d2ef5835e3abe8c5
padma67 <60962682+padma67@users.noreply.github.com>
2020-03-13 12:55:41 +0530

    Create check_k_exist_in_array.js

---
+++ Javascript/codekata/basics/check_k_exist_in_array.js
@@ -0,0 +1,29 @@
+const readline = require ('readline');
+const inp=readline.createInterface({
+  input:process.stdin
+});
+const m=[];
+inp.on("line",(data)=>{
+  m.push(data);
+});
+inp.on("close",()=>{
+  var a=m[0].split(" ");
+  var k=parseInt(a[1]);
+  var b=m[1].split(" ");
+  var count=0;
+  for(i=0;i<b.length;i++)
+  {
+    if(k==b[i].split(" "))
+    {
+      count=1;
+    }
+  }
+  if(count==1)
+  {
+      console.log("yes");
+  }
+  else
+  {
+      console.log("no");
+  }
+});
