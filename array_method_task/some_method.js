c39cd318f821144e0bc41cbac1ee9e9d5052c6d6
padma67 <60962682+padma67@users.noreply.github.com>
2020-05-07 13:07:05 +0530

    Create some_method.js

---
+++ array_method_task/some_method.js
@@ -0,0 +1,15 @@
+//check if any one elemnt in array is give 1,when its divided by 2
+const fun1 = (array, callback) => {
+  let a=[],count=0;
+  for(var i=0;i<array.length;i++){
+    if(callback(array[i]))
+      {
+        return true
+      }
+  }
+}
+const somefun =(value)=>value%2==1
+fun1([2,3,5,8,12], somefun) 
+
+//output
+//true
