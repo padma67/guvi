3127d05c44e99647e95473b6ad2af0bc5038fcf6
padma67 <60962682+padma67@users.noreply.github.com>
2020-05-07 12:50:37 +0530

    Create every_method.js

---
+++ array_method_task/every_method.js
@@ -0,0 +1,22 @@
+
+const fun1 = (array, callback) => {
+  let a=[],count=0;
+  for(var i=0;i<array.length;i++){
+    if(callback(array[i]))
+      {
+        count+=1;
+      }
+  }
+  if(count==array.length){
+    return true
+  }
+  else
+    {
+      return false
+    }
+}
+const mapfun =(value)=>value>0
+fun1([2,3,5,8,12], mapfun) 
+
+//output
+true
