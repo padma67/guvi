34ac69bff0e15d72062768f3f28d682e3ef56c0e
padma67 <60962682+padma67@users.noreply.github.com>
2020-05-07 12:59:51 +0530

    Create findIndex.js

---
+++ array_method_task/findIndex.js
@@ -0,0 +1,14 @@
+const fun1 = (array, callback) => {
+  let a=[],count=0;
+  for(var i=0;i<array.length;i++){
+    if(callback(array[i]))
+      {
+        return i
+      }
+  }
+}
+const indfun =(value)=>value==5
+fun1([2,3,5,8,12], indfun) 
+
+//output
+2
