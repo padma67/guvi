ebb4f2eaa539f2d2452ffbd6f486d2506476c59a
padma67 <60962682+padma67@users.noreply.github.com>
2020-06-03 07:16:21 +0530

    Create Problem7.js

---
+++ Day2:(02-06-2020)/Problem7.js
@@ -0,0 +1,18 @@
+//problem 7
+//compare 2 objects are equal or not using JSON.stringify():
+
+//2 given objects
+var expected = {foo: 5, bar: 6};
+var actual = {foo: 5, bar: 6};
+
+//function to compare 2 objects and return results 
+function assertObjectsEqual(actual, expected, testName){
+//check 2 objects are equal or not
+  if(JSON.stringify(actual)==JSON.stringify(expected)){
+  	console.log("Passed");
+  }
+  else{
+  	console.log("FAILED [myTest] Expected "+ JSON.stringify(actual) +" but got "+JSON.stringify(expected));
+  }
+}
+assertObjectsEqual(actual, expected, 'detects that two objects are equal');
