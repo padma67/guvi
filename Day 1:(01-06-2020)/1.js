4829d4183895486c01a92cf2847177b7e4dafafa
padma67 <60962682+padma67@users.noreply.github.com>
2020-07-04 13:39:00 +0530

    Update 1.js

--- Day 1:(01-06-2020)/1.js
+++ Day 1:(01-06-2020)/1.js
@@ -2,8 +2,4 @@
 
 //The function return a specification in image as object
 var Technical_Specification;
-function object(){
 Technical_Specification={Dimensions:{"Overall length":{mm:3500},"Overall width":{mm:1400},"Oveallh height":{mm:1490},"Wheelbase":{mm:2360},"Track width":{Front:{mm:1405},Rear:{mm:1400}},"Minimum turning radius":{m:"4.5"},"Minimum ground clearance":{mm:170}},Capacities:{"Seating capacity":{persons:5},"Fuel tank capacity":{litre:35}},Engine:{Type:"KB-Series","Number of cylinders":3,"Number of valves":12,capacity:{"cc/cm3":998},"Bore x stroke":{mm:"73*79.5"},"Compression ratio":"10:1","Maximum power":{"PS/rpm":"67/6,200"},"Maximum torque":{"Nm/rpm":"90/3,500"},"Fuel distribution":"Multipoint injection"},Transmission:{Type:"5-speed MT"},Chassis:{Steering:["rack & pinion" ,"Power assisted"],Brakes:{Front:"Ventilated discs",Rear:"Drums"},Suspension:{Front:"MacPherson strut & coil spring",Rear:"Isolated trailing link & coil spring"},"Shock absorbers":"Gas filled","Tyre_(tubeless)":"155/80R13"},Weights:{"Kerb weight (min,with full opttions)":{kg:"860-880"},"Gross vehicle weight":{kg:"1,320"}}};
-return Technical_Specification
-}
-object();
